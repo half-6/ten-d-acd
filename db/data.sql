7b0ac28d0c3c150e8d5d8525ab3c63e8f1440023
Cyokin Zhang <half_6@hotmail.com>
2019-03-06 18:33:14 -0800

    机器类型更新 #44

--- db/data.sql
+++ db/data.sql
@@ -2,4 +2,4 @@ TRUNCATE public.cancer_type, public.machine_type RESTART IDENTITY;
 insert into public.cancer_type(cancer_type_name,cancer_type_short_name,status)
 values ('Thyroid nodules','TH','active'),('Breast tumours','BR','active'),('Prostate lesions','PR','deleted'),('Kidney lesions','KD','deleted'),('Lymphoma','LY','deleted');
 insert into public.machine_type(machine_type_name)
-values ('Philips'),('Samsung');
+values ('Philips'),('Samsung'),('Siemens'),('Toshiba'),('GE'),('eSaote');
