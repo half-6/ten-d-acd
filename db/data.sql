7fc07f4b4ce3b5406d23dcdd042de959796ae8d0
Cyokin Zhang <half_6@hotmail.com>
2019-03-09 23:46:11 -0800

    update localization copy

--- db/data.sql
+++ db/data.sql
@@ -1,10 +1,10 @@
 TRUNCATE public.cancer_type, public.machine_type RESTART IDENTITY;
 insert into public.cancer_type(cancer_type_name,cancer_type_short_name,cancer_type_chinese_name,status)
-values ('Thyroid nodules','TH','结节性甲状腺肿','active'),
-       ('Breast tumours','BR','乳腺肿块','active'),
-       ('Prostate lesions','PR','前列腺病变','deleted'),
-       ('Kidney lesions','KD','肾脏病变','deleted'),
-       ('Lymphoma','LY','淋巴瘤','deleted');
+values ('Thyroid nodules','TH','甲状腺癌','active'),
+       ('Breast tumours','BR','乳腺癌','active'),
+       ('Prostate lesions','PR','前列腺癌','deleted'),
+       ('Kidney lesions','KD','肾脏癌','deleted'),
+       ('Lymphoma','LY','淋巴癌','deleted');
 insert into public.machine_type(machine_type_name,machine_type_chinese_name)
 values ('Philips','飞利浦'),
        ('Samsung','三星'),
