c09d1bb887290e3fd6235b1bda3e2facebc16fed
Cyokin Zhang <half_6@hotmail.com>
2019-04-15 15:59:06 -0700

    分离about page #78

--- client/frontend/src/views/about-page/index.js
+++ client/frontend/src/views/about-page/index.js
@@ -1,46 +1,14 @@
-import _ from "lodash";
-import api from '@/api';
-
 export default {
-    name: "list-page",
+    name: "about-page",
     data() {
         return {
-            aggCancerTypeList: [],
-            totalDiagnostics: 0,
-            totalTP: 0,
-            totalTN: 0,
-            totalFP: 0,
-            totalFN: 0,
-            accuracy: 0,
-            tpr: 0,
-            tnr: 0,
-            precision: 0,
+
         };
     },
     methods: {
-        async agg() {
-            const r = await api.getAggTable()
-            if (r.data) {
-                this.aggCancerTypeList = r.data;
-                _.each(this.aggCancerTypeList,o=>{
-                    o.accuracy = (o.tp + o.tn)/(o.tp + o.fp + o.tn + o.fn);
-                    o.tpr = o.tp/(o.tp + o.fn);
-                    o.tnr = o.tn/(o.fp + o.tn);
-                    o.precision = o.tp/(o.tp + o.fp);
-                })
-                this.totalDiagnostics = _.sumBy(this.aggCancerTypeList, 'number_diagnostics');
-                this.totalTP = _.sumBy(this.aggCancerTypeList, 'tp');
-                this.totalTN = _.sumBy(this.aggCancerTypeList, 'tn');
-                this.totalFP = _.sumBy(this.aggCancerTypeList, 'fp');
-                this.totalFN = _.sumBy(this.aggCancerTypeList, 'fn');
-                this.accuracy = (this.totalTP + this.totalTN)/(this.totalTP + this.totalFP + this.totalTN + this.totalFN);
-                this.tpr = this.totalTP/(this.totalTP + this.totalFN);
-                this.tnr = this.totalTN/(this.totalFP + this.totalTN);
-                this.precision = this.totalTP/(this.totalTP + this.totalFP);
-            }
-        },
+
     },
     mounted: function () {
-        this.agg()
+
     }
 };
--- client/frontend/src/views/about-page/index.vue
+++ client/frontend/src/views/about-page/index.vue
@@ -8,64 +8,6 @@
             <h1><img class="tend-logo" :src="require('../../assets/i/tenDlogo.png')" >{{$t('about.title')}}</h1>
             <p class="lead clearfix">{{$t('about.desc')}}</p>
             <hr class="my-4">
-            <div class="row">
-              <div class="col table-responsive">
-                <table class="table table-striped table-hover">
-                  <thead>
-                    <tr>
-                    <th scope="col">{{$t('about.table-cancer-type')}}</th>
-                    <th scope="col">{{$t('about.table-diagnostics')}}</th>
-                    <th scope="col">{{$t('about.table-lowest-process-time')}}</th>
-                    <th scope="col">{{$t('about.table-fastest-process-time')}}</th>
-                    <th scope="col">{{$t('about.table-average-process-time')}}</th>
-                    <th scope="col">{{$t('about.table-tp')}}</th>
-                    <th scope="col">{{$t('about.table-tn')}}</th>
-                    <th scope="col">{{$t('about.table-fp')}}</th>
-                    <th scope="col">{{$t('about.table-fn')}}</th>
-                    <th scope="col">{{$t('about.table-accuracy')}}</th>
-                    <th scope="col">{{$t('about.table-tpr')}}</th>
-                    <th scope="col">{{$t('about.table-tnr')}}</th>
-                    <th scope="col">{{$t('about.table-precision')}}</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                    <tr v-for="item in aggCancerTypeList">
-                      <td scope="row">{{item.cancer_type_name}}</td>
-                      <td scope="row">{{item.number_diagnostics| number-format('0,0')}}</td>
-                      <td scope="row">{{item.max | number-format('0.[0000]')}}</td>
-                      <td scope="row">{{item.min | number-format('0.[0000]')}}</td>
-                      <td scope="row">{{item.avg | number-format('0.[0000]')}}</td>
-                      <td scope="row">{{item.tp | number-format('0.[0000]')}}</td>
-                      <td scope="row">{{item.tn | number-format('0.[0000]')}}</td>
-                      <td scope="row">{{item.fp | number-format('0.[0000]')}}</td>
-                      <td scope="row">{{item.fn | number-format('0.[0000]')}}</td>
-                      <td scope="row">{{item.accuracy | number-format('0.[00]%')}}</td>
-                      <td scope="row">{{item.tpr | number-format('0.[00]%')}}</td>
-                      <td scope="row">{{item.tnr | number-format('0.[00]%')}}</td>
-                      <td scope="row">{{item.precision | number-format('0.[00]%')}}</td>
-                    </tr>
-                  </tbody>
-                  <tfoot>
-                    <tr>
-                      <th>{{$t('about.table-total')}}</th>
-                      <th>{{totalDiagnostics | number-format('0,0')}}</th>
-                      <th></th>
-                      <th></th>
-                      <th></th>
-                      <th>{{totalTP | number-format('0,0')}}</th>
-                      <th>{{totalTN | number-format('0,0')}}</th>
-                      <th>{{totalFP | number-format('0,0')}}</th>
-                      <th>{{totalFN | number-format('0,0')}}</th>
-                      <th>{{accuracy | number-format('0.[00]%')}}</th>
-                      <th>{{tpr | number-format('0.[00]%')}}</th>
-                      <th>{{tnr | number-format('0.[00]%')}}</th>
-                      <th>{{precision | number-format('0.[00]%')}}</th>
-                    </tr>
-                  </tfoot>
-                </table>
-
-              </div>
-            </div>
             <p class="pt-3">{{$t('about.app-version')}}:{{$tendConfig.applicationVersion}}</p>
             <p>{{$t('about.recognition-version')}}:{{$tendConfig.recognitionVersion}}</p>
             <p class="power"><img :src="require('../../assets/i/logo.png')" >{{$t('about.power-by')}}</p>
