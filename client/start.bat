09a6ccb5bde572661e6f6fd6b74f9fe2aad10104
Cyokin Zhang <half_6@hotmail.com>
2019-04-15 01:36:50 -0700

    version 0.0.11 release

--- client/start.bat
+++ client/start.bat
@@ -1,2 +1,2 @@
 SET image.recognition.path=D:\codes\qyotech\ten-d-acd\client\backend\lib\Image_Recognition.jar
-java -jar backend\target\com.tend.acd.backend-0.0.10-SNAPSHOT.jar
+java -jar backend\target\com.tend.acd.backend-0.0.11-SNAPSHOT.jar
