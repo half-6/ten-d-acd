75b74bd1f162ee45c36dcd2b931bbaa83abe8b55
Cyokin Zhang <half_6@hotmail.com>
2019-04-22 01:57:31 -0700

    release 0.0.12

--- client/release-docker.bat
+++ client/release-docker.bat
@@ -1,2 +1,2 @@
-"D:\tools\winscp517\pscp" -v -pw bbh2gd "backend\target\com.tend.acd.backend-0.0.11-SNAPSHOT.jar" "cyokin@192.168.201.110:/mnt/tend/tend/com.tend.acd.backend.jar"
-"D:\tools\winscp517\plink" -v -ssh -t cyokin@192.168.201.110 -pw bbh2gd "cd /mnt/tend/; sudo docker-compose build; sudo docker tag cyokin/tend cyokin/tend:0.0.11; sudo docker push cyokin/tend:0.0.11"
+"D:\tools\winscp517\pscp" -v -pw bbh2gd "backend\target\com.tend.acd.backend-0.0.12-SNAPSHOT.jar" "cyokin@192.168.201.110:/mnt/tend/tend/com.tend.acd.backend.jar"
+"D:\tools\winscp517\plink" -v -ssh -t cyokin@192.168.201.110 -pw bbh2gd "cd /mnt/tend/; sudo docker-compose build; sudo docker tag cyokin/tend cyokin/tend:0.0.12; sudo docker push cyokin/tend:0.0.12"
--- client/start-demo.bat
+++ client/start-demo.bat
@@ -1,3 +1,3 @@
 SET image.recognition.path=D:\codes\qyotech\ten-d-acd\backend\lib\Image_Recognition.jar
-java -jar backend\target\com.tend.acd.backend-0.0.11-SNAPSHOT.jar  --spring.profiles.active=demo
+java -jar backend\target\com.tend.acd.backend-0.0.12-SNAPSHOT.jar  --spring.profiles.active=demo
 
