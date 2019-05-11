1bf9e8186df5fd487e0594055536744c7498bf91
Cyokin Zhang <half_6@hotmail.com>
2019-05-11 11:45:47 -0700

    机器品牌增加“声科”/“Supersonic”及“日立”/“Hitachi” #104

--- db/data.sql
+++ db/data.sql
@@ -2,17 +2,20 @@
 INJECT DATE
 *****************************/
 TRUNCATE public.cancer_type, public.machine_type RESTART IDENTITY CASCADE;
-insert into public.cancer_type(cancer_type_name,cancer_type_short_name,cancer_type_chinese_name,status)
-values ('Thyroid nodules','TH','甲状腺癌','active'),
-       ('Breast tumours','BR','乳腺癌','active'),
-       ('Prostate lesions','PR','前列腺癌','deleted'),
-       ('Kidney lesions','KD','肾脏癌','deleted'),
-       ('Lymphoma','LY','淋巴癌','deleted');
-insert into public.machine_type(machine_type_name,machine_type_chinese_name)
-values ('Philips','飞利浦'),
-       ('Samsung','三星'),
-       ('Siemens','西门子'),
-       ('Toshiba','东芝'),
-       ('GE','通用电气'),
-       ('eSaote','百胜');
+insert into public.cancer_type(cancer_type_id,cancer_type_name,cancer_type_short_name,cancer_type_chinese_name,status)
+values (1,'Thyroid nodules','TH','甲状腺癌','active'),
+       (2,'Breast tumours','BR','乳腺癌','active'),
+       (3,'Prostate lesions','PR','前列腺癌','deleted'),
+       (4,'Kidney lesions','KD','肾脏癌','deleted'),
+       (5,'Lymphoma','LY','淋巴癌','deleted');
+insert into public.machine_type(machine_type_id, machine_type_name,machine_type_chinese_name)
+values (1,'Philips','飞利浦'),
+       (2,'Samsung','三星'),
+       (3,'Siemens','西门子'),
+       (4,'Toshiba','东芝'),
+       (5,'GE','通用电气'),
+       (6,'eSaote','百胜'),
+       (7,'Supersonic','声科'),
+       (8,'Hitachi','日立')
+       ;
 INSERT INTO public.hospital(hospital_name,hospital_chinese_name) values ('Renji Hospital','仁济医院');
