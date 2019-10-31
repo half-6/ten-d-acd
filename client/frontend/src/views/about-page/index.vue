d6fa8440e1859c2c6cac608149e34b301c161e61
Cyokin Zhang <half_6@hotmail.com>
2019-10-31 13:53:33 -0700

    update version to 1.0.0

--- client/frontend/src/views/about-page/index.vue
+++ client/frontend/src/views/about-page/index.vue
@@ -8,8 +8,10 @@
             <h1><img class="tend-logo" :src="require('../../assets/i/tenDlogo.png')" >{{$t('about.title')}}</h1>
             <p class="lead clearfix">{{$t('about.desc')}}</p>
             <hr class="my-4">
-            <p class="pt-3">{{$t('about.app-version')}}:{{$tendConfig.applicationVersion}}</p>
-            <p>{{$t('about.recognition-version')}}:{{$tendConfig.recognitionVersion}}</p>
+<!--            <p class="pt-3">{{$t('about.app-version')}}:{{$tendConfig.applicationVersion}}</p>-->
+<!--            <p>{{$t('about.recognition-version')}}:{{$tendConfig.recognitionVersion}}</p>-->
+            <p class="pt-3">{{$t('about.app-version')}}:1.0.0</p>
+            <p>{{$t('about.recognition-version')}}:1.0.0</p>
             <p>{{$t('about.expire-on')}}:{{new Date($tendConfig.certificateEntity.expire_date)}}</p>
             <p class="power"><img :src="require('../../assets/i/logo.png')" >{{$t('about.power-by')}}</p>
           </div>
